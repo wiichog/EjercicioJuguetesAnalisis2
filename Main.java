52370907cd7e92ebcf165c973dfbe2710eae5c12
Santiago Solórzano <tiagosansopa@gmail.com>
2016-07-19 12:24:53 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,6 +15,9 @@ public class Main
 	{
 		int salida = 0, opcion= 0;
 		Scanner entrada = new Scanner(System.in);
+		//Arreglos de objetos
+		Juguete [] juguetes = new Juguete [100];
+		Proveedor [] proveedores = new Proveedor [100];
 		
 		while(salida!=10)
 		{
@@ -29,20 +32,41 @@ public class Main
 			System.out.println("8. Dado el nombre de un proveedor conocer cuántos juguetes mecánicos o electrónicos suministra.");
 			System.out.println("9. Dado un tipo de juguete, listar todos los proveedores que suministran ese tipo de juguete.");
 			System.out.println("10. Salir");
-			opcion = reader.nextInt();
+			opcion = entrada.nextInt();
 			
 			switch (opcion) 
 			{
 				case 1:  
+					System.out.println("Ingreso de Nuevo Juguete");
+
 					System.out.println("Ingrese el codigo");
+					String codigo = entrada.nextLine();
 					System.out.println("Ingrese la marca");
-						 break;
-				case 2:  
-						 break;
+					String marca = entrada.nextLine();
+					System.out.println("Ingrese tipo de juguete");
+					String tipoDeJuguete = entrada.nextLine();
+					System.out.println("Ingrese edad");
+					int edad = entrada.nextInt();
+					System.out.println("Ingrese el proveedor del juguete");
+					int proveedor = entrada.nextInt();
+					System.out.println("Ingrese el valor unitario");
+					int valorUnitario = entrada.nextInt();
+					System.out.println("Ingrese la complejidad");
+					int complejidad = entrada.nextInt();
+				break;
+				
+				case 2:
+				break;
+				
 				case 3:  
-						 break;
+				break;
+				
 				case 4: 
-						 break;
+					System.out.println("Ingreso de Nuevo Proveedor");
+					
+					System.out.println("Ingrese el nombre");
+					String nombreProveedor = entrada.nextLine();
+				break;
 				case 5:  
 						 break;
 				case 6:  
